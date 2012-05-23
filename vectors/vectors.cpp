53d4e8b9b4b0c3dd6793ac93b1896e22f0ad2b9b
Matthew <matthew.jack@gmail.com>
2012-05-23 14:37:27 +0200

    Added and used Recast vectors

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -9,6 +9,7 @@
 #include "Win32specific.h"
 #include "Cry_Vector3.h"
 #include "VecMath.h"
+#include "Recast.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -21,6 +22,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	Vec3f B2 = {1,2,3};
 	Vec3f B3 = B + B2;
 
+	float C[3] = {1,1,1};
+	float C2[3] = {1,2,3};
+	float C3[3];
+
+	rcVadd(C3, C, C2);
 
 	return 0;
 }
